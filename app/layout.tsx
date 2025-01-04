fce56158fa04c1dee015ce8a835b86b92a55332e
shtanriverdi <shtanriverdi@yahoo.com>
2025-01-04 18:49:48 +0300

    Implement side nav bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import SideNav from "@/app/ui/sidenav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+          <div className="w-full flex-none md:w-64">
+            <SideNav />
+          </div>
+          <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+        </div>
       </body>
     </html>
   );
---
+++ app/ui/sidenav.tsx
@@ -0,0 +1,25 @@
+import NavLinks from "@/app/ui/nav-links";
+import { PowerIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+
+export default function SideNav() {
+  return (
+    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+      <div className="mb-2 flex h-20 items-end justify-start rounded-md bg-sky-100 md:h-40">
+        <div className="flex items-center justify-center w-full h-full">
+          <Image src="/s4e-light.webp" alt="logo" width={128} height={128} />
+        </div>
+      </div>
+      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        <NavLinks />
+        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        <form>
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            <PowerIcon className="w-6" />
+            <div className="hidden md:block">Sign Out</div>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
