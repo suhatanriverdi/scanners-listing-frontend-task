eb3fec36ad516701943b34c979ada009cee63ebd
shtanriverdi <shtanriverdi@yahoo.com>
2025-01-05 15:33:15 +0300

    Fix logo image aspect ratio warning

--- app/ui/sidenav.tsx
+++ app/ui/sidenav.tsx
@@ -7,7 +7,13 @@ export default function SideNav() {
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <div className="mb-2 flex h-20 items-end justify-start rounded-md bg-sky-100 md:h-40">
         <div className="flex items-center justify-center w-full h-full">
-          <Image src="/s4e-light.webp" alt="logo" width={128} height={128} />
+          <Image
+            src="/s4e-light.webp"
+            alt="logo"
+            width={265}
+            height={102}
+            style={{ width: "auto", height: "auto" }}
+          />
         </div>
       </div>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
